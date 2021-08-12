81f5f9e5f9e730ec877e92ff4d1d0fc0d1d86448
Yanoro <felipepassosvale@gmail.com>
2021-08-12 14:25:43 -0300

    Changed GUI Data type and increased safety checks

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,7 @@ import Data.Bifunctor (second)
 
 {- TODO:
   Add component safety checks
+  Split main file
 -}
 
 data WindowType = Default
@@ -33,8 +34,12 @@ data Window = Window {
   components :: [(Component, Rectangle CInt, Window -> Window)]
 }
 
+data GUI = GUI {
+  windows :: [Window],
+  lastWindowID :: ID
+}
+
 type ID = Int
-type GUI = [Window]
 type Color = SDL.V4 Word8
 
 red :: Color
@@ -55,8 +60,22 @@ green = SDL.V4 32 194 14 0
 fontSize :: CInt
 fontSize = 12
 
+emptyGUI :: GUI
+emptyGUI = GUI { windows = [], lastWindowID = 0 }
+
 maxDimension :: Int
 maxDimension = 32768
+
+createWindow :: ID -> Rectangle CInt -> CInt -> Color ->
+             Color -> [(Component, Rectangle CInt, Window -> Window)] -> (Window, ID)
+createWindow wID dmensions tBarHeight bColor tBarColor comps =
+  (Window { windowID = wID, dimensions = dmensions,
+           beingDragged = False, borderColor = bColor, titleBarFillColor = tBarColor,
+           titleBarHeight = tBarHeight, components = comps}, wID + 1)
+
+newGUIWindow :: Window -> GUI -> GUI
+newGUIWindow w gui = let prevWindowList = windows gui in
+                  gui { windows = mappend [w] prevWindowList }
 
 splitSurface :: SDL.Surface -> CInt -> CInt -> IO [SDL.Surface]
 splitSurface srcSurf width height = mapM (\(x, dx) -> do
@@ -81,7 +100,7 @@ createTextComponent render msg color font = do
   freeSurface surf
   return $ Text msg color texts
 
-
+{- Gets adequate source and destination rectangles to properly display text -}
 srcDestRects :: Int -> [(SDL.Texture, SDL.TextureInfo)] -> Rectangle CInt -> [((SDL.Texture, Rectangle CInt), Rectangle CInt)]
 srcDestRects msgLength texts rect = let dRecs = destRects msgLength rect
                                         sRecs = srcRects msgLength texts dRecs in
@@ -168,7 +187,7 @@ drawWindow render font w = do
   mapM_ (drawComponent render font) comps
 
 drawGUI :: SDL.Renderer -> SDL.Font.Font -> GUI -> IO ()
-drawGUI render font = mapM_ (drawWindow render font)
+drawGUI render font gui = mapM_ (drawWindow render font) (windows gui)
 
 getFirst :: (a -> Bool) -> [a] -> Maybe a
 getFirst _ [] = Nothing
@@ -183,17 +202,17 @@ insideRectangle :: Rectangle CInt -> SDL.Point SDL.V2 CInt -> Bool
 insideRectangle (Rectangle (SDL.P (SDL.V2 xStart yStart)) (SDL.V2 width height)) (SDL.P (SDL.V2 x y)) =
   not $ x < xStart || y < yStart || x > xStart + width || y > yStart + height
 
-clickInsideGUI :: SDL.Point SDL.V2 CInt -> GUI -> Maybe Window
+clickInsideGUI :: SDL.Point SDL.V2 CInt -> [Window] -> Maybe Window
 clickInsideGUI click = getFirst (\x -> dimensions x `insideRectangle` click)
 
 setDragging :: Window -> GUI -> GUI
 setDragging oldWindow = updateGUI (oldWindow {beingDragged = True})
 
 -- Whether there is a window currently being dragged
-windowBeingDragged :: GUI -> Bool
+windowBeingDragged :: [Window] -> Bool
 windowBeingDragged = any beingDragged
 
-getDraggedWindow :: GUI -> Maybe Window
+getDraggedWindow :: [Window] -> Maybe Window
 getDraggedWindow = getFirst beingDragged
 
 gotSomething :: Maybe a -> Bool
@@ -213,10 +232,12 @@ windowAddComponent w rect comp answerFunction =
 {- The windowID of the newWindow has to be the same as the old one.
    This way we don't need to keep passing a targetID to the function just to "update" the gui -}
 updateGUI :: Window -> GUI -> GUI
-updateGUI _ [] = []
-updateGUI newWindow (window:rest) = if windowID window == windowID newWindow
-                                       then newWindow : rest
-                                       else window : updateGUI newWindow rest
+updateGUI newWindow gui = let oldWindows = windows gui in
+                            gui {windows = go oldWindows} where
+  go [] = []
+  go (window:rest) = if windowID window == windowID newWindow
+                     then newWindow : rest
+                     else window : go rest
 
 moveRectangle :: (CInt, CInt) -> Rectangle CInt -> Rectangle CInt
 moveRectangle (dx, dy) (Rectangle (SDL.P (SDL.V2 x y)) botCorner) = Rectangle (SDL.P (SDL.V2 (x + dx) (y + dy))) botCorner
@@ -233,20 +254,22 @@ updateDraggedWindow (SDL.V2 dx dy) draggedWindow = let (Rectangle (SDL.P (SDL.V2
 guiHandleClick :: SDL.InputMotion -> SDL.MouseButton -> SDL.Point SDL.V2 CInt -> GUI -> GUI
 guiHandleClick motion button clickCoords oldGUI | motion == SDL.Pressed
                                                   && button == SDL.ButtonLeft =
-                                                          case clickInsideGUI clickCoords oldGUI of
+                                                          case clickInsideGUI clickCoords guiWindows of
                                                             Nothing -> oldGUI
                                                             Just w -> setDragging w oldGUI
                                                 | motion == SDL.Released
                                                   && button == SDL.ButtonLeft
-                                                  && windowBeingDragged oldGUI =
-                                                          let draggedWindow = fromJust $ getDraggedWindow oldGUI in
+                                                  && windowBeingDragged guiWindows =
+                                                          let draggedWindow = fromJust $ getDraggedWindow guiWindows in
                                                             updateGUI (draggedWindow { beingDragged = False }) oldGUI
                                                 | otherwise = oldGUI
+                                            where guiWindows = windows oldGUI
 
 guiHandleMotion :: SDL.V2 CInt -> GUI -> GUI
-guiHandleMotion motion oldGUI = if windowBeingDragged oldGUI
-                                then updateGUI (updateDraggedWindow motion (fromJust $ getDraggedWindow oldGUI)) oldGUI
+guiHandleMotion motion oldGUI = if windowBeingDragged guiWindows
+                                then updateGUI (updateDraggedWindow motion (fromJust $ getDraggedWindow guiWindows)) oldGUI
                                 else oldGUI
+                              where guiWindows = windows oldGUI
 
 guiHandleEvent :: SDL.EventPayload -> GUI -> GUI
 guiHandleEvent (SDL.MouseButtonEvent (SDL.MouseButtonEventData _ motion _ button _ clickCoords)) oldGUI =
@@ -268,7 +291,7 @@ loop render font gui = do
         case SDL.eventPayload event of
           SDL.MouseButtonEvent (SDL.MouseButtonEventData _ motion _ button _ coords)->
                             if motion == SDL.Pressed && button == SDL.ButtonLeft
-                            then gotSomething $ clickInsideGUI (fmap fromIntegral coords) gui
+                            then gotSomething $ clickInsideGUI (fmap fromIntegral coords) (windows gui)
                             else False
 
           _ -> False
@@ -302,10 +325,11 @@ main = do
   SDL.Font.setHinting font SDL.Font.Mono
   size <- SDL.Font.getHinting font
   print size
-  let startGUI = [Window { windowID = 1, dimensions = Rectangle (SDL.P (SDL.V2 0 0)) (SDL.V2 500 500),
-                           beingDragged = False, borderColor = white, titleBarFillColor = blue,
-                           titleBarHeight = 10, components = []}]
-      newGUI = updateGUI (windowAddComponent (head startGUI) (Rectangle (SDL.P (SDL.V2 5 5)) (SDL.V2 490 440)) text id) startGUI
+  let (startWindow, nextID) = createWindow 1 (Rectangle (SDL.P (SDL.V2 0 0)) (SDL.V2 500 500))
+                              10 white blue []
+
+      startGUI = newGUIWindow startWindow emptyGUI
+      newGUI = updateGUI (windowAddComponent (head $ windows startGUI) (Rectangle (SDL.P (SDL.V2 5 5)) (SDL.V2 490 440)) text id) startGUI
 
   loop renderer font newGUI
   SDL.destroyWindow window
